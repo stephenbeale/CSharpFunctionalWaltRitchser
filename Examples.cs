6439ebb32e503503ec75925e76e497d973ac7657
Steve Beale <beale.stephen@gmail.com>
2024-08-20 09:27:58 +0100

    Add project files.

---
+++ Examples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFunctionalWaltRitchser
+{
+    public class Examples
+    {
+        public decimal CalcDiscount(decimal amount, decimal discountRate)
+        {
+            //Functional because return is based entirely on input parameters.
+            return amount * (1 - discountRate);
+        }
+    }
+}
