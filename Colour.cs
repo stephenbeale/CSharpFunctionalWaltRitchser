19c348d523cd5217fa744adc596821ad4240412d
Steve Beale <beale.stephen@gmail.com>
2024-08-20 13:52:32 +0100

    Branch 03-03? Obsolete ways of initialising properties.

---
+++ Colour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFunctionalWaltRitchser
+{
+    public sealed class Colour
+    {
+        //Obsolete - don't use this
+        public byte Orange { get; private set; }
+
+        //Better but more verbose
+        //Backing field which must be set in the constructor as there is no setter
+        private readonly byte _purple { get; set; }
+
+        public byte Purple { 
+            get { return _purple; }
+        }
+    }
+}
