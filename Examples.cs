4f102f404d07dda184c3be729c556053e8c5ea1d
Steve Beale <beale.stephen@gmail.com>
2024-08-20 16:51:56 +0100

    End of day.

--- Examples.cs
+++ Examples.cs
@@ -20,11 +20,14 @@ namespace CSharpFunctionalWaltRitchser
             Colour colour1 = new Colour.Builder().Create(); //default
             Colour colour2 = new Colour.Builder().Red(127).Create(); //red only
             Colour colour3 = new Colour.Builder().Green(45).Alpha(255).Create(); // green and alpha
+            Colour colour4 = new Colour.Builder().Blue(1).Create();
 
             var greenValue = colour3.Green;
 
+            //Can't set it
             //colour2.Red = 0;
 
+            //But existing methods still work.
             Colour lightenedColour = colour3.Lighten(22);
 
         }
