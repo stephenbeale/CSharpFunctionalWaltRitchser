0d73ebc067dc1e0c7e2a91182d4599659879c54f
Steve Beale <beale.stephen@gmail.com>
2024-08-20 12:27:33 +0100

    Me playing a bit more with these.

--- Program.cs
+++ Program.cs
@@ -1,27 +1,20 @@
 ﻿using CSharpFunctionalWaltRitchser;
 
+using System.Collections.Immutable;
+
 class Program
 {
     static void Main(string[] args)
     {
-        //Timer t = new Timer(TimerCallback, null, 0, 100);
-        //Console.ReadLine();
 
-        ////Me playing with named arguments
-        //DateTime dt1 = new DateTime(month: 1, day: 7, year: 2027);
-        //DateTime dt2 = new DateTime(month: 11, day: 4, year: 2056);
-        //var dateTimeNow = DateTime.Compare(dt1, dt2);
+        var examples = new Examples();
+        ImmutableList<int> myImmutableList = ImmutableList.Create(1, 2, 3);
+        //This is makes no changes to list
+        var result = examples.AddNumbersToList(myImmutableList);
+        Console.WriteLine($"Result of first add numbers: {result.Count()}");
+
+        //This is also fine as it creates a new list on the fly
+        var myNewImmutableList = examples.AddNumbersToList(myImmutableList);
+        Console.WriteLine($"Result of second add numbers: {myNewImmutableList.Count()}");  
     }
-
-    //private static void TimerCallback(Object o) 
-    //{
-    //    var examples = new Examples();
-    //    Console.Clear();
-
-    //    //Makes this function impure since time is always different
-    //    Console.WriteLine(DateTime.Now);
-    //    var result =
-    //        examples.GetCurrentTimeRoundedUpToCustomMinuteInterval(4, DateTime.Now);
-    //    Console.WriteLine(result);
-    //}
 }
