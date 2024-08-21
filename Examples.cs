71a5ee0c57c501903e170bc1ea0da03608517b5f
Steve Beale <beale.stephen@gmail.com>
2024-08-21 15:04:42 +0100

    Expression examples in full

--- Examples.cs
+++ Examples.cs
@@ -21,5 +21,41 @@ namespace CSharpFunctionalWaltRitchser
          * - simple name e.g. name of variable, type member, method parameter, namespace or type.
          * 
          */
+
+        public void ExamplesOfExpressions()
+        {
+            /*
+             * An expression yields a value
+             * and can be used in places where a value is expected
+             */
+
+            //variables hold the value
+            string stringValue;
+            int intValue;
+
+            intValue = Int32.MaxValue;
+
+            // literals are expressions
+
+            stringValue = "hello";
+            intValue = 909;
+
+            // variables are expressions
+            string sayHello = stringValue;
+            int x = intValue;
+
+            // invocations (function calls that return a value)
+            var upperCaseWord = stringValue.ToUpper();
+            var concatValue = $"first number{intValue}, second number{x}";
+
+            double calculatedValue = Math.Sqrt(Math.Abs(3) + x);
+
+            // operators and operands
+
+            bool isBig = x > 1000;
+            string isBigString = isBig ? "Big Number" : "Small number";
+
+
+        }
     }
 }
