ba05876308c6df629f76a3cf88e36f11ce310341
Steve Beale <beale.stephen@gmail.com>
2024-08-22 15:36:37 +0100

    Pipelining example - static extension class, all methods are then static. Does not need an object to call those methods therefore.

--- Examples.cs
+++ Examples.cs
@@ -15,9 +15,30 @@ namespace CSharpFunctionalWaltRitchser
 {
     public class Examples
     {
-
-        public void DoWork()
+        public void DoWorkWithPipeline()
         {
+            int value = 5;
+
+            int resultA = value.ToFourthPower().MakeNegative();
+            int resultB = value.ToFourthPower().MakeNegative().AddTo(10);
+        }
+    }
+
+    public static class Extensions
+    {
+        public static int ToFourthPower(this int candidate)
+        {
+            return candidate * candidate * candidate * candidate;
+        }
+
+        public static int MakeNegative(this int candidate)
+        {
+            return candidate * -1;
+        }
+
+        public static int AddTo(this int candidate, int adder)
+        {
+            return candidate + adder;
         }
     }
 }
